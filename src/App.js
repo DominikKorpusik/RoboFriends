f52c0f8e67dd7d5f8c8d1c8fe0bc2b2f0b9cfb38
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-06-28 23:10:27 +0000

    Adding Comments

--- src/App.js
+++ src/App.js
@@ -13,10 +13,20 @@ class App extends Component {
         }
     }
 
+    /* The `onSearchChange` function is an event handler that is called when the search input field
+    changes. It takes an `event` parameter, which represents the event that triggered the function
+    (in this case, the change event on the search input field). */
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
     }
 
+    /**
+     * The render function filters a list of robots based on a search field and renders a card list
+     * component with the filtered robots.
+     * @returns The render() method is returning a JSX element. It is a div element with a class name
+     * of "tc" (text center), containing an h1 element with the text "RoboFriends", a SearchBox
+     * component with a prop of searchChange, and a CardList component with a prop of robots.
+     */
     render() {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
