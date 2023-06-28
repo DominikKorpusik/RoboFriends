5317590dbc5251398c958ebd616a759cbab56753
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-06-28 23:01:08 +0000

    Making changes in appliactio tree, creating new modue App, modifiying Card, CardList, Adding new SearchBox

--- src/Card.js
+++ src/Card.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Card extends Component {
-    render() {
-        const { id, name, username, email } = this.props;
-        return (
-            <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-                <img alt="robot" src={`https://robohash.org/${id}?200x200`}></img>
-                <div>
-                    <h2>{name}</h2>
-                    <p>{username}</p>
-                    <p>{email}</p>
-                </div>
+
+const Card = ({ id, name, username, email }) => {
+    return (
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="robot" src={`https://robohash.org/${id}?200x200`}></img>
+            <div>
+                <h2>{name}</h2>
+                <p>{username}</p>
+                <p>{email}</p>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Card;
--- src/CardList.js
+++ src/CardList.js
@@ -1,27 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import Card from "./Card";
 
-class CardList extends Component {
-    render() {
-        const robots = this.props.robots;
-        return (
-            <>
-                {
-                    robots.map((user, i) => {
-                        return (
-                            <Card
-                                key={robots[i].id}
-                                id={robots[i].id}
-                                name={robots[i].name}
-                                username={robots[i].username}
-                                email={robots[i].email}
-                            />
-                        )
-                    })
-                }
-            </>
-        );
-    }
+const CardList = ({ robots }) => {
+    return (
+        <>
+            {
+                robots.map((user, i) => {
+                    return (
+                        <Card
+                            key={robots[i].id}
+                            id={robots[i].id}
+                            name={robots[i].name}
+                            username={robots[i].username}
+                            email={robots[i].email}
+                        />
+                    )
+                })
+            }
+        </>
+    );
 }
 
 export default CardList;
