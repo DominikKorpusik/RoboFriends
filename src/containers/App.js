50feec6a2ad5a80d5716af5c5a799a288552d250
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-07-01 15:01:26 +0000

    Changing files tree, adding ErrorBoundry.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import CardList from './CardList';
-import SearchBox from './SearchBox.js';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox.js';
 import './App.css';
-import Scroll from './Scroll.js';
+import Scroll from '../components/Scroll.js';
+import ErrorBoundry from "../components/ErrorBoundry";
 
 
 class App extends Component {
@@ -41,28 +42,28 @@ class App extends Component {
      * component with a prop of searchChange, and a CardList component with a prop of robots.
      */
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
 
-        if (this.state.robots.length === 0) {
-            return (
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <h2 className="f2">Loading</h2>
-                </div>
-            )
-        } else {
-            return (
+        return !robots.length ? (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <h2 className="f2">Loading</h2>
+            </div>
+        ) :
+            (
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
-        }
     }
 }
 
